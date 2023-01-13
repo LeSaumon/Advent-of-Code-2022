c9c9f707f511a24a6d4209375c5c7a257fa09982
Simon LEBRUN <sl.simonlebrun0@gmail.com>
2023-01-13 16:57:40 +0100

    Removed useless comments

--- scripts/day_4-2.py
+++ scripts/day_4-2.py
@@ -1,20 +1,3 @@
-# Il semble qu'il y ait encore pas mal de travail en double de prévu.
-# Au lieu de cela, les Elfes aimeraient connaître le nombre de paires qui se chevauchent tout court.
-
-# Dans l'exemple ci-dessus, les deux premières paires (2-4,6-8 et 2-3,4-5)
-# ne se chevauchent pas, tandis que les quatre autres paires (5-7,7-9, 2-8,3-7, 6-6,4-6 et 2-6,4-8)
-# se chevauchent :
-
-#     5-7,7-9 se chevauche dans une seule section, 7.
-#     2-8,3-7 chevauche toutes les sections 3 à 7.
-#     6-6,4-6 se chevauche dans une seule section, 6.
-#     2-6,4-8 chevauche les sections 4, 5 et 6.
-
-# Ainsi, dans cet exemple, le nombre de paires d'affectations qui se chevauchent est de 4.
-
-# Dans combien de paires d'affectations les plages se chevauchent-elles ?
-
-
 import os
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
--- scripts/day_5-1.py
+++ scripts/day_5-1.py
@@ -1,81 +1,12 @@
-# # # # L'expédition peut partir dès que les dernières fournitures ont été déchargées des navires.
-# # # # Les fournitures sont stockées dans des piles de caisses marquées, mais comme les fournitures nécessaires
-# # # # sont enfouies sous de nombreuses autres caisses, celles-ci doivent être réorganisées.
-
-# # # # Le navire est équipé d'une grue géante capable de déplacer les caisses entre les piles.
-# # # # Pour s'assurer qu'aucune caisse ne soit écrasée ou ne tombe, le grutier les réarrange en une série d'étapes soigneusement planifiées.
-# # # # Une fois les caisses réarrangées, les caisses souhaitées se trouveront en haut de chaque pile.
-
-# # # # Les Elfes ne veulent pas interrompre le grutier pendant cette procédure délicate,
-# # # # mais ils ont oublié de lui demander quelle caisse se retrouvera à quel endroit,
-# # # # et ils veulent être prêts à les décharger dès que possible pour pouvoir embarquer.
-
-# # # # Ils disposent toutefois d'un dessin des piles de caisses de départ et de la procédure de réarrangement (votre contribution au puzzle).
-# # # #  Par exemple :
-
-# # # #     [D]
-# # # # [N] [C]
-# # # # [Z] [M] [P]
-# # # #  1   2   3
-
-# # # # move 1 from 2 to 1
-# # # # move 3 from 1 to 3
-# # # # move 2 from 2 to 1
-# # # # move 1 from 1 to 2
-
-# # # # Dans cet exemple, il y a trois piles de caisses. La pile 1 contient deux caisses : la caisse Z est en bas et la caisse N est en haut.
-# # # # La pile 2 contient trois caisses ; de bas en haut, ce sont les caisses M, C et D.
-# # # # Enfin, la pile 3 contient une seule caisse, P.
-
-# # # # Ensuite, la procédure de réarrangement est donnée.
-# # # # À chaque étape de la procédure, une quantité de casiers est déplacée d'une pile à une autre.
-# # # # Lors de la première étape de la procédure de réarrangement ci-dessus, une caisse est déplacée de la pile 2 à la pile 1,
-# # # #  ce qui donne la configuration suivante :
-
-# # # # [D]
-# # # # [N] [C]
-# # # # [Z] [M] [P]
-# # # #  1   2   3
-
-# # # Dans la deuxième étape, trois caisses sont déplacées de la pile 1 à la pile 3.
-# # # Les caisses sont déplacées une par une, de sorte que la première caisse à être déplacée (D)
-# # #  se retrouve sous les deuxième et troisième caisses :
-
-# # #         [Z]
-# # #         [N]
-# # #     [C] [D]
-# # #     [M] [P]
-# # #  1   2   3
-
-# # Ensuite, les deux caisses sont déplacées de la pile 2 à la pile 1.
-# # Encore une fois, comme les caisses sont déplacées une par une, la caisse C se retrouve sous la caisse M :
-
-# #         [Z]
-# #         [N]
-# # [M]     [D]
-# # [C]     [P]
-# #  1   2   3
-
-# # Enfin, une caisse est déplacée de la pile 1 à la pile 2 :
-# #         [Z]
-# #         [N]
-# #         [D]
-# # [C] [M] [P]
-# #  1   2   3
-
-# Les lutins ont juste besoin de savoir quelle caisse se retrouvera sur le dessus de chaque pile ;
-# dans cet exemple, les caisses du dessus sont C dans la pile 1, M dans la pile 2 et Z dans la pile 3,
-# vous devez donc les combiner ensemble et donner aux lutins le message CMZ.
-
-# Une fois la procédure de réorganisation terminée, quelle caisse se retrouve en haut de chaque pile ?
-
 from collections import deque
 import os
 import re
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+
 def extract_data_from_move(move):
     return re.findall('[0-9]+', move)
+
 matrix_input = [
     ["S", "L", "W"],
     ["J", "T", "N", "Q"],
